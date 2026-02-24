e684fa9063ec746093d61ade302240779d0d1955
Steve2370 <briceyouatchui@gmail.com>
2026-02-23 21:18:45 -0500

    feat: admin panel + signalement system

--- backend/src/Interfaces/EmailProviderInterface.php
+++ backend/src/Interfaces/EmailProviderInterface.php
@@ -4,6 +4,6 @@ namespace App\Interfaces;
 
 interface EmailProviderInterface
 {
-    public function sendEmail(string $to, string $subject, string $body, bool $isHtml = true): bool;
+    public function sendEmail(string $to, string $subject, string $body, bool $isHtml = true, ?string $replyTo = null): bool;
     public function getLastError(): ?string;
 }
