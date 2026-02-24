f7a7ffb256ee3f26d4ad3e2799ea15c8c9187cc2
Steve2370 <briceyouatchui@gmail.com>
2026-02-24 12:00:54 -0500

    feat: admin panel + signalement system

--- frontend/src/contexts/common/Navbar.jsx
+++ frontend/src/contexts/common/Navbar.jsx
@@ -85,7 +85,7 @@ export default function Navbar() {
                     </div>
 
                     <div className="flex justify-center">
-                        <div className="w-full max-w-md -ml-12">
+                        <div className="w-full max-w-md -ml-10">
                             <form onSubmit={handleSearch} className="relative w-full">
                                 <div className="relative">
                                     <input
