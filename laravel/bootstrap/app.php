87f73074712fe32183f84dc3b261e2a20f64610b
Steve2370 <pougouebrice@gmail.com>
2026-07-26 17:21:10 -0400

    fix: exclude api routes from CSRF verification

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\OptionalSanctumAuth;
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
         $middleware->api(prepend: [
             EnsureFrontendRequestsAreStateful::class,
             OptionalSanctumAuth::class,
